8cdfdef9feb1d1707e79c83b059f68e472aa39ef
sylns9 <116550430+sylns9@users.noreply.github.com>
2022-10-24 17:25:11 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oc22O5wUYr6Vfgoc_vfFwnkBAYvQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'mHpqPextSaYK1y0TfZo-ceP06cAIulkNp452RrVRu6s',
+      useTemplateId: 'i8_EhpIazsRrQUV1ibnQ6EWLcU_AOPY1xHXmpoq39W0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-11',
       festivals: [
@@ -39,13 +39,18 @@ const USER_CONFIG = {
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2018-10-25' },
+        { keyword: 'comprehensive_horoscope' },
+        { keyword: 'love_horoscope' },
+        { keyword: 'career_horoscope' },
+        { keyword: 'wealth_horoscope' },
+        { keyword: 'healthy_horoscope.DATA' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'mHpqPextSaYK1y0TfZo-ceP06cAIulkNp452RrVRu6s',
+  CALLBACK_TEMPLATE_ID: 'paqzu1iNtPVRFNb0yivxfPUzoAcUgprBq2nghCJ7YWg',
 
   CALLBACK_USERS: [
     {
