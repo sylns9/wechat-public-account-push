dffdbc6ccfb8cf71ad98f8f34774989358657666
sylns9 <116550430+sylns9@users.noreply.github.com>
2022-10-24 16:48:27 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '思文',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oc22O5y92-y3tY6fXCo1SRhaSsgA',
+      id: 'oc22O5wUYr6Vfgoc_vfFwnkBAYvQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'mHpqPextSaYK1y0TfZo-ceP06cAIulkNp452RrVRu6s',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
